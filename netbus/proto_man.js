2cbb9363e6a41231f6e883039027bc4ba09a2ede
tree <hongbo@szljfkj.com>
2018-01-29 15:45:57 +0800

    buf，json协议
    buf和json注册协议解码

--- netbus/proto_man.js
+++ netbus/proto_man.js
@@ -1,42 +1,52 @@
 let netbus = require("./netbus.js")
 let log = require('../utils/log.js')
-/*规定
-1，服务号和命令号不能为零 data部分不能为空
-2，服务号和命令号大小不能超过2个字节的整数
-3，buf协议里面2个字节来存放服务号，命令号;0开始的服务号，1开始的命令号
-4，加密解密
-5, 服务号和命令号都用小尾
-*/
+    /*规定
+    1，服务号和命令号不能为零 data部分不能为空
+    2，服务号和命令号大小不能超过2个字节的整数
+    3，buf协议里面2个字节来存放服务号，命令号;0开始的服务号，1开始的命令号
+    4，加密解密
+    5, 服务号和命令号都用小尾
+    */
 let proto_man = {};
-//加密函数
-function encrypt_cmd_buf(str_or_buf){
+
+//buf协议的编码/解码管理   stype,ctype -->encoder/decoder
+//解码器  buf协议的所有解码函数
+let decoders = {
+
+    }
+    //编码器  buf协议的所有编码函数
+let encoders = {
+
+    }
+    //加密函数
+function encrypt_cmd_buf(str_or_buf) {
     return str_or_buf
 }
 //解密函数
-function decrypt_cmd_buf(str_or_buf){
+function decrypt_cmd_buf(str_or_buf) {
     return str_or_buf
 }
 
 //编码函数
-function _json_encode(stype,ctype,body){
+function _json_encode(stype, ctype, body) {
     let cmd = {}
-       cmd[0] = stype;
-       cmd[1] = ctype;
-       cmd[2] = body;
-    let str =   JSON.stringify(cmd) 
-    //此处可以做加密  
+    cmd[0] = stype;
+    cmd[1] = ctype;
+    cmd[2] = body;
+    let str = JSON.stringify(cmd)
+        //此处可以做加密  
     return str
 }
 //解码函数
-function json_decode(cmd_json){
-    let cmd=JSON.parse(cmd_json)
+function json_decode(cmd_json) {
+    let cmd = JSON.parse(cmd_json)
     console.log(cmd)
-    if(!cmd || !cmd[0] || 
-        !cmd[1] || 
+    if (!cmd || !cmd[0] ||
+        !cmd[1] ||
         typeof(cmd[0]) == 'underfined' ||
         typeof(cmd[1]) == 'underfined' ||
-        typeof(cmd[2]) == 'underfined' ){ //cmd解码错误 cmd[0]不存在 cmd[1]不存在
-           return null
+        typeof(cmd[2]) == 'underfined') { //cmd解码错误 cmd[0]不存在 cmd[1]不存在
+        return null
     }
     return cmd
 }
@@ -46,22 +56,24 @@ function json_decode(cmd_json){
 //body       数据
 //编码函数
 //返回后是一段编码过的数据
-function encode_cmd(proto_type,stype,cmd_type,body){
+function encode_cmd(proto_type, stype, cmd_type, body) {
     let buf = null;
-    if(proto_type == netbus.PROTO_JSON){
-        buf = _json_encode(stype,cmd_type,body)
-        // return encrypt_cmd_buf(str)
-    }else{
+    if (proto_type == netbus.PROTO_JSON) {
+        buf = _json_encode(stype, cmd_type, body)
+            // return encrypt_cmd_buf(str)
+    } else {
         //buf协议
-        let key = get_key(stype,cmd_type)
-        if(!encoders[key]){
+        log.info(proto_type, stype, cmd_type, body)
+        let key = get_key(stype, cmd_type)
+        if (!encoders[key]) {
+            console.log(1)
             return null
         }
         buf = encoders[key](body)
     }
     //空不加密
-    if(buf){
-      buf = encrypt_cmd_buf(buf)
+    if (buf) {
+        buf = encrypt_cmd_buf(buf)
     }
     //end
     return buf;
@@ -71,53 +83,50 @@ function encode_cmd(proto_type,stype,cmd_type,body){
 // 返回 {0：是type，1，ctype,2:body}
 //解码函数
 //返回的是一段解码好的数据
-function decode_cmd(proto_type,str_or_buf){
+function decode_cmd(proto_type, str_or_buf) {
     let str_or_bufs = decrypt_cmd_buf(str_or_buf)
-   
-    if(proto_type == netbus.PROTO_JSON){
+
+    if (proto_type == netbus.PROTO_JSON) {
         log.info(str_or_bufs)
-      return json_decode(str_or_bufs)
+        return json_decode(str_or_bufs)
     }
     let cmd = null;
-    let stype = str_or_bufs.readUInt16Le(0)
-    let ctype = str_or_bufs.readUInt16Le(2)
-    let key = get_key(stype,ctype)
-    if(decoders[key]){
+    let stype = str_or_bufs.readUInt16LE(0)
+    let ctype = str_or_bufs.readUInt16LE(2)
+        // log.error(stype, ctype)
+    let key = get_key(stype, ctype)
+        // log.error(key)
+    if (!decoders[key]) {
+        log.error(2)
         return null; //无解码函数
     }
+    // log.error(decoders[key])
     let buf = decoders[key](str_or_buf)
-    buf = encrypt_cmd_buf(buf)
-    return buf;
+    log.error(buf)
+    cmd = encrypt_cmd_buf(buf)
+    return cmd;
 }
 
-//buf协议的编码/解码管理   stype,ctype -->encoder/decoder
-//解码器  buf协议的所有解码函数
-let decoders = {
 
-}
-//编码器  buf协议的所有编码函数
-let encoders = {
-
-}
 //产生key的函数  自定义公式的 也可以用随机字符串
-function get_key(stype,ctype){
-   return (stype*65536 + ctype)
+function get_key(stype, ctype) {
+    return (stype * 65536 + ctype)
 }
 //编码器函数注册 encode_func(body) return 二进制buffer对象
-function reg_buf_encoder(stype,ctype,encode_func){
-   let key = get_key(stype,ctype)
-   if(encoders[key]){ //已经注册过了，是否错了
-      log.warn("stype"+stype+','+ ctype + "is reged!")
-   }
-   encoders[key] = encode_func
+function reg_buf_encoder(stype, ctype, encode_func) {
+    let key = get_key(stype, ctype)
+    if (encoders[key]) { //已经注册过了，是否错了
+        log.warn("stype" + stype + ',' + ctype + "is reged!")
+    }
+    encoders[key] = encode_func
 }
 //解码器函数注册 decode_func(cmd_buf) return cmd{0:服务号，1：命令号，2:body}
-function reg_buf_decoder(stype,ctype,decode_func){
-    let key = get_key(stype,ctype)
-   if(decoders[key]){ //已经注册过了，是否错了
-      log.warn("stype"+stype+','+ ctype + "is reged!")
-   }
-   decoders[key] = decode_func
+function reg_buf_decoder(stype, ctype, decode_func) {
+    let key = get_key(stype, ctype)
+    if (decoders[key]) { //已经注册过了，是否错了
+        log.warn("stype" + stype + ',' + ctype + "is reged!")
+    }
+    decoders[key] = decode_func
 }
 proto_man.encode_cmd = encode_cmd
 proto_man.decode_cmd = decode_cmd
--- test/test_proto_man.js
+++ test/test_proto_man.js
@@ -6,75 +6,87 @@ let proto_man = require("../netbus/proto_man.js")
 
 //json 编码和解码
 let data = {
-    uname:"blake",
-    upwd:"123456"
-}
-let buf = proto_man.encode_cmd(netbus.PROTO_JSON,1,1,data)
-log.info(buf)
-log.error("json length",buf.length)
+        uname: "blake",
+        upwd: "123456"
+    }
+    // let buf = proto_man.encode_cmd(netbus.PROTO_JSON, 1, 1, data)
+    //     // log.info(buf)
+    //     // log.error("json length", buf.length)
 
-let cmd = proto_man.decode_cmd(netbus.PROTO_JSON,buf)
-log.info(cmd)
+// let cmd = proto_man.decode_cmd(netbus.PROTO_JSON, buf)
+// log.info(cmd)
 //end
 
 //buf 编码和解码
-function encode_cmd_1_1(body){
+function encode_cmd_1_1(body) {
     let stype = 1;
     let ctype = 1;
-    let total_len =2 +2 +body.uname.length + body.upwd.length +2 + 2;
+    let total_len = 2 + 2 + body.uname.length + body.upwd.length + 2 + 2;
     let buffer = Buffer.allocUnsafe(total_len)
-       buffer.writeUInt16LE(0,stype)
-       buffer.writeUInt16LE(2,ctype)
-       //uname 字符串
-       buffer.writeUInt16LE(body.uname.length,4)
-       buffer.write(body.uname,6)
-       //upwd 字符串
-       let offset = 6 + body.uname.length
-       buffer.writeUInt16LE(body.upwd.length,offset)
-       buffer.write(body.upwd,offset + 2)
-
-       return buf
+    buffer.writeUInt16LE(stype, 0)
+    buffer.writeUInt16LE(ctype, 2)
+        //uname 字符串
+    buffer.writeUInt16LE(body.uname.length, 4)
+    buffer.write(body.uname, 6)
+        //upwd 字符串
+    let offset = 6 + body.uname.length
+    buffer.writeUInt16LE(body.upwd.length, offset)
+        // log.error(body.upwd)
+        // log.error(offset + 2)
+    buffer.write(body.upwd, offset + 2)
+        // log.error(buffer)
+    return buffer
 
 }
 
-function decode_cmd_1_1(cmd_buf){
+function decode_cmd_1_1(cmd_buf) {
     let stype = 1
     let ctype = 1
-    //uname
+        //uname
+
     let uname_len = cmd_buf.readUInt16LE(4)
+
     //服务号加命令号加uname的长度+2比cmd_buf.length大就是有问题
-    if((uname_len +2 + 2+ 2) >= cmd_buf.length){
-       return null
+    if ((uname_len + 2 + 2 + 2) > cmd_buf.length) {
+        return null
     }
 
-    let uname = cmd_buf.toString("utf8",6,6+uname_len)
-    if(!nuame){
+    let uname = cmd_buf.toString("utf8", 6, 6 + uname_len)
+    log.info(uname)
+    if (!uname) {
         return null
     }
 
     let offset = 6 + uname_len
-    let upwd_len = cmd_buf.cmd_buf.readUInt16LE(offset)
-    if((offset +upwd_len+ 2) >= cmd_buf.length){
-        return null
-     }
-     let upwd = cmd_buf.toString("utf8",offset + 2,offset + 2 + upwd_len) 
-     
-     cmd = {
-         0:1,
-         1:1,
-         2:{
-             "uanme":uname,
-             "upwd":ipwd
-         }
-     }
 
-     return cmd
+    let upwd_len = cmd_buf.readUInt16LE(offset)
+
+    if ((offset + upwd_len + 2) > cmd_buf.length) {
+        log.error(3)
+        return null
+    }
+
+    let upwd = cmd_buf.toString("utf8", offset + 2, offset + 2 + upwd_len)
+        // let upwd = cmd_buf.toString("utf8", 13, 19)
+    let cmd = {
+        0: 1,
+        1: 1,
+        2: {
+            "uname": uname,
+            "upwd": upwd
+        }
+    }
+
+    return cmd
 }
 
-proto_man.reg_encoder(encode_cmd_1_1)
-proto_man.reg_decoder(decode_cmd_1_1)
-//end
+proto_man.reg_encoder(1, 1, encode_cmd_1_1)
+proto_man.reg_decoder(1, 1, decode_cmd_1_1)
+    //end
 
-let proto_cmd_buf = proto_man.encode_cmd(netbus.PROTO_BUF,data)
+let proto_cmd_buf = proto_man.encode_cmd(netbus.PROTO_BUF, 1, 1, data)
 log.info(proto_cmd_buf)
 log.error(proto_cmd_buf.length)
+let cmd = proto_man.decode_cmd(netbus.PROTO_BUF, proto_cmd_buf)
+log.info(cmd)
+    // log.error(cmd.length)
